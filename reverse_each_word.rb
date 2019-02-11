646ef4b0f425de52fa194e41be655aa64cff8a20
richardtam.ny <richardtam.ny@gmail.com>
2019-02-11 20:56:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
     arr1 = sentence.split(" ")
     reversed = []
-    arr1.each do |x|
+    arr1.collect do |x|
       reversed.push(x.reverse)
     end
       reversed.join(" ")
